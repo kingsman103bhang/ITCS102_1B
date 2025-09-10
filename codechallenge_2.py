aead914d7a3f6c2592a94642924680d9c6c33248
Marc Aeron A. Villeno <marcvilleno@gmail.com>
2025-09-10 15:55:27 +0800

    Add files via upload

---
+++ codechallenge_2.py
@@ -0,0 +1,13 @@
+print("Welcome Manga Fan's are you looking to read")
+print("heres the list of genre \nAction \nComedy \nDrama \nRomance")
+genre = input("Enter your choice : ").lower()
+if genre == "action":
+    print("Here's the list \nNaruto \nAttack on titan \nOne Piece \nDemon Slayer \nMy Hero Academia")
+    input("Select your choice")
+    if=="Naruto":
+elif genre == "comedy":
+     print("Here's the list \nGintama \nBokonopiko \nToradora \nLucky Star \nSchool Rumble")
+elif genre == "drama":
+     print("Here's the list \nYour Lie In April \nOrange \nAnohan \nClannad \nA Silent Voice")
+elif genre == "romance":
+     print("Here's the list \nGolden time /nFruit Basket \nFalse LOve \nYour Name \nHaru Ride")
