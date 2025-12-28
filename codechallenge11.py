29aaf2f8798c3f16076b2c349f80492918ed9f9f
Marc Aeron A. Villeno <marcvilleno@gmail.com>
2025-12-28 13:31:52 +0800

    Add files via upload

---
+++ codechallenge11.py
@@ -0,0 +1,9 @@
+print("\t\t x",end=" ")
+for aeron in range(1,11,1):
+    for h in range(10, aeron, -1):
+        print(" ", end=" ")
+    for g in range(1, aeron, 1):
+        print("x", end=" ")
+    for i in range(1, aeron,1):
+        print("x", end=" ")
+    print()
